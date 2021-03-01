34a52dad6d0dd5c48f5d1d14786f15ca3cb20f23
jsf <jsfsarkis@gmail.com>
2021-03-01 11:06:48 +0200

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:ride_share/screens/home_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final FirebaseApp app = await Firebase.initializeApp(
+    name: 'db2',
+    options: Platform.isIOS || Platform.isMacOS
+        ? FirebaseOptions(
+      appId: '1:808635764507:ios:9b49254f833ef7dbc70dbe',
+      apiKey: 'AIzaSyDh8hy76eR38E9Qr7i4s23_W6pdKm8c5Gs',
+      projectId: 'ride-sharing-90212',
+      messagingSenderId: '808635764507',
+      databaseURL: 'https://ride-sharing-90212-default-rtdb.firebaseio.com',
+    )
+        : FirebaseOptions(
+      appId: '1:808635764507:android:22a3316263f88284c70dbe',
+      apiKey: 'AIzaSyBMWlvNTB8F5PWPoEK-7yvX4g5GuZaSiE0',
+      messagingSenderId: '808635764507',
+      projectId: 'ride-sharing-90212',
+      databaseURL: 'https://ride-sharing-90212-default-rtdb.firebaseio.com',
+    ),
+  );
   runApp(MyApp());
 }
 
@@ -12,6 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String id = 'home_screen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
