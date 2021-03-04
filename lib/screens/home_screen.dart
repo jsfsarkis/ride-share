416648fc4752bcf869214ceea5a225a95d39f480
jsf <jsfsarkis@gmail.com>
2021-03-04 10:52:15 +0200

    implement sliding drawer on the main page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
 import 'package:ride_share/components/divider_line.dart';
-import 'package:ride_share/constants.dart';
+
+import '../constants.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = 'home_screen';
@@ -22,10 +23,100 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Container(
+        width: 250,
+        color: Colors.white,
+        child: Drawer(
+          child: ListView(
+            padding: EdgeInsets.all(0.0),
+            children: [
+              Container(
+                height: 160,
+                color: Colors.white,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/user_icon.png',
+                        height: 60.0,
+                        width: 60.0,
+                      ),
+                      SizedBox(width: 15.0),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Joseph Sarkis',
+                            style:
+                                TextStyle(fontSize: 20, fontFamily: BoldFont),
+                          ),
+                          SizedBox(height: 5.0),
+                          Text('View Profile'),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              DividerLine(),
+              SizedBox(height: 10.0),
+              ListTile(
+                leading: Icon(OMIcons.cardGiftcard),
+                title: Text(
+                  'Free Rides',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.creditCard),
+                title: Text(
+                  'Payments',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.history),
+                title: Text(
+                  'Ride History',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.contactSupport),
+                title: Text(
+                  'Support',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(OMIcons.info),
+                title: Text(
+                  'About',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Stack(
         children: [
           GoogleMap(
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.height / 2.89),
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height / 2.89),
             mapType: MapType.normal,
             myLocationButtonEnabled: true,
             initialCameraPosition: _kGooglePlex,
