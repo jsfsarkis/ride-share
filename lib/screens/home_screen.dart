3646bce4ead945a14e0824457d7d18203913f9e9
jsf <jsfsarkis@gmail.com>
2021-03-08 15:49:23 +0200

    remove current location text from map screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -256,10 +256,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                                geocodingService.pickupAddress.placeName != null
-                                    ? geocodingService.pickupAddress.placeName
-                                    : 'Add Home'),
+                            Text('Add Home'),
                             SizedBox(height: 3),
                             Text(
                               'Your residential address',
