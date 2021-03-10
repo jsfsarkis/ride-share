b3f760dc30463e165b4a2ca1038cbb046c44199e
jsf <jsfsarkis@gmail.com>
2021-03-10 12:58:44 +0200

    implement getPlaceDetails, get coordinates for a specific place by id

--- lib/components/prediction_tile.dart
+++ lib/components/prediction_tile.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
-import 'package:ride_share/models/address_model.dart';
+import 'package:provider/provider.dart';
 import 'package:ride_share/models/address_prediction_model.dart';
-import 'package:ride_share/services/network_service.dart';
+import 'package:ride_share/services/places_service.dart';
 
 import '../constants.dart';
 
@@ -11,68 +11,77 @@ class PredictionTile extends StatelessWidget {
   final AddressPredictionModel addressPrediction;
   PredictionTile({this.addressPrediction});
 
-  void getPlaceDetails(String placeId) async {
-    String url =
-        'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=$mapKey';
-
-    var response = await NetworkService.httpGetRequest(url);
-
-    if (response == 'failed') {
-      return;
-    }
-
-    if (response['status'] == 'OK') {
-      AddressModel thisPlace = AddressModel();
-      thisPlace.placeName = response['result']['name'];
-      thisPlace.id = placeId;
-      thisPlace.latitude = response['result']['geometry']['location']['lat'];
-      thisPlace.latitude = response['result']['geometry']['location']['lng'];
-    }
-  }
+  // void getPlaceDetails(String placeId, BuildContext context) async {
+  //   String url =
+  //       'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=$mapKey';
+  //
+  //   var response = await NetworkService.httpGetRequest(url);
+  //
+  //   if (response == 'failed') {
+  //     return;
+  //   }
+  //
+  //   if (response['status'] == 'OK') {
+  //     AddressModel thisPlace = AddressModel();
+  //     thisPlace.placeName = response['result']['name'];
+  //     thisPlace.id = placeId;
+  //     thisPlace.latitude = response['result']['geometry']['location']['lat'];
+  //     thisPlace.latitude = response['result']['geometry']['location']['lng'];
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
+    var placesService = Provider.of<PlacesService>(context);
     return SingleChildScrollView(
-      child: Container(
-        child: Column(
-          children: [
-            SizedBox(height: 8.0),
-            Row(
-              children: [
-                Icon(OMIcons.locationOn, color: colorDimText),
-                SizedBox(width: 12.0),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        addressPrediction.mainText != null
-                            ? addressPrediction.mainText
-                            : '',
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(fontSize: 18.0, fontFamily: BoldFont),
-                      ),
-                      SizedBox(height: 2.0),
-                      Text(
-                        addressPrediction.secondaryText != null
-                            ? addressPrediction.secondaryText
-                            : '',
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontSize: 12.0,
-                          color: colorDimText,
-                          fontFamily: BoldFont,
+      child: TextButton(
+        onPressed: () {
+          placesService.getPlaceDetails(addressPrediction.placeId, context);
+        },
+        child: Container(
+          child: Column(
+            children: [
+              SizedBox(height: 8.0),
+              Row(
+                children: [
+                  Icon(OMIcons.locationOn, color: colorDimText),
+                  SizedBox(width: 12.0),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          addressPrediction.mainText != null
+                              ? addressPrediction.mainText
+                              : '',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                              fontSize: 18.0,
+                              fontFamily: BoldFont,
+                              color: Colors.black),
                         ),
-                      ),
-                    ],
+                        SizedBox(height: 2.0),
+                        Text(
+                          addressPrediction.secondaryText != null
+                              ? addressPrediction.secondaryText
+                              : '',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: colorDimText,
+                            fontFamily: BoldFont,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 8.0),
-          ],
+                ],
+              ),
+              SizedBox(height: 8.0),
+            ],
+          ),
         ),
       ),
     );
--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:ride_share/components/progress_dialog.dart';
 import 'package:ride_share/models/address_model.dart';
 import 'package:ride_share/models/address_prediction_model.dart';
 
@@ -23,6 +26,7 @@ class PlacesService extends ChangeNotifier {
     this.destinationAddress.longitude = longitude;
     this.destinationAddress.placeFormattedAddress = placeFormattedAddress;
     this.destinationAddress.isPlaceName = isPlaceName;
+    notifyListeners();
   }
 
   Future<dynamic> searchPlace(String placeName) async {
@@ -49,6 +53,37 @@ class PlacesService extends ChangeNotifier {
           notifyListeners();
         }
       }
+    }
+  }
+
+  void getPlaceDetails(String placeId, BuildContext context) async {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) =>
+          ProgressDialog(status: 'Please wait...'),
+    );
+
+    String url =
+        'https://maps.googleapis.com/maps/api/place/details/json?placeid=$placeId&key=$mapKey';
+
+    var response = await NetworkService.httpGetRequest(url);
+
+    Navigator.pop(context);
+
+    if (response == 'failed') {
+      return;
+    }
+
+    if (response['status'] == 'OK') {
+      setDestinationAddress(
+        id: placeId,
+        placeName: response['result']['name'],
+        latitude: response['result']['geometry']['location']['lat'],
+        longitude: response['result']['geometry']['location']['lng'],
+      );
+
+      print(destinationAddress.placeName);
     }
   }
 }
